dcb188cb1f92a6623cb771b364e747e012c2420a
beirti <beirtipol@gmail.com>
2019-02-17 20:24:35 +0000

    Directory creation script

---
+++ prepareDirectories.sh
@@ -0,0 +1,8 @@
+mkdir -p /apps/data/mysql-server
+mkdir -p /apps/data/teamcity-server
+mkdir -p /apps/logs/teamcity-server
+mkdir -p /apps/data/teamcity-agent/conf
+mkdir -p /apps/data/teamcity_agent/docker_volumes
+mkdir -p /apps/data/gitlab-ce/config
+mkdir -p /apps/logs/gitlab-ce
+mkdir -p /apps/data/gitlab-ce/data
